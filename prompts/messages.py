2fe172bc3bf0d9d88d5ceb7a917955a0a23f4ac6
jethibhavya <jethibhavya@gmail.com>
2026-01-19 14:11:52 +0530

    latest commits

--- prompts/messages.py
+++ prompts/messages.py
@@ -1,10 +1,16 @@
-from langchain.core_messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 messages = [
     SystemMessage(content="You are a helpful assistant that writes a poem based on the user's input."),
+    HumanMessage(content="Write a poem about the beauty of nature."),
 ]
+
+chain = model | StrOutputParser()
+response = chain.invoke(messages)
+print(response)
