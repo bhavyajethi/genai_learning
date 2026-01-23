048a613a73480314be45fdc82eced1097ed20b25
jethibhavya <jethibhavya@gmail.com>
2026-01-23 14:10:25 +0530

    latest commits

--- str_output/typedict_demo.py
+++ str_output/typedict_demo.py
@@ -1,14 +1,24 @@
-# TypedDict lets us define the structure of a dictionary
+# TypedDict lets us define the expected structure of a dictionary
 from typing import TypedDict
 
-class Person(TypedDict): 
-    name: str 
-    age: int
 
-p : Person = {"name":"John", "age": 22}
+class Person(TypedDict):
+    name: str     # Required key "name" with string value
+    age: int      # Required key "age" with integer value
 
-# Running both the codes will cause the 2nd code to throw an error as the 1st one converts dict to int by data annotation and 2nd tries to do an item assign which throws an error. If u comment the 1st code and run the 2nd it successfully updates the age in the dict and returns it.
+
+p: Person = {"name": "John", "age": 22}  # Create a Person-typed dictionary
+
+# NOTE: ":" is for type hints; "=" is for assignment; p["key"] = value updates dict data
+
+
+# This is a TYPE ANNOTATION + ASSIGNMENT, not a dict update
+# It reassigns p to an int (24), replacing the original dictionary
 p: Person['age'] = 24
 print("This is the 1st p", p)
+
+
+# This now fails because p is no longer a dict (it's an int)
+# Correct way to update the dictionary value is: p["age"] = 24
 p['age'] = 24
 print("This is the second p", p)
