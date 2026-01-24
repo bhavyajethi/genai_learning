afadf6b44a2e3f446857b080700c83a139fa7ef8
jethibhavya <jethibhavya@gmail.com>
2026-01-24 14:05:34 +0530

    latest commits

--- str_output/str_op_typedict.py
+++ str_output/str_op_typedict.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from typing import Optional, TypedDict, Annotated, Literal
 
 load_dotenv()
-model = ChatGoogleGenerativeAI()
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
 class Review(TypedDict):
     themes: Annotated[list[str], "List all the key themes discusses in the review in a list"]
     summary: Annotated[str, "Write a summary of the review"]
@@ -12,4 +12,21 @@ class Review(TypedDict):
     cons: Annotated[Optional[list[str]], "List all the cons of the review in a list"]
     name: Annotated[Optional[str], "Write the name of the reviewer of the summary"]
 
-    
+structured_model = model.with_structured_output(Review)
+
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Bhavya Jethi
+""")
+
+print(result['name'])
