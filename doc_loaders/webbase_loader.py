e71da0f2177512853195b18476f6fa42c57aa86d
jethibhavya <jethibhavya@gmail.com>
2026-02-14 12:34:31 +0530

    latest commits

--- doc_loaders/webbase_loader.py
+++ doc_loaders/webbase_loader.py
@@ -1,12 +1,12 @@
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI()
+model = ChatGoogleGenerativeAI()
 
 prompt = PromptTemplate(
     template='Answer the following question \n {question} from the following text - \n {text}',
